478045d8f3373016f369a78ff1a21460537b5593
jobairRafi96 <jubairrafi96960@gmail.com>
2020-04-18 15:25:27 +0600

    condition added

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -11,3 +11,16 @@ var imgsrc2 = "images/" + diceimg2;
 
 document.querySelector(".img1").setAttribute("src",imgsrc1);
 document.querySelector(".img2").setAttribute("src",imgsrc2);
+if (randomNumber1 > randomNumber2) {
+
+  document.querySelector("h1").innerHTML = "Player One Wins";
+
+}
+else if (randomNumber1< randomNumber2) {
+
+  document.querySelector("h1").innerHTML = "Player two wins";
+
+}
+else{
+  document.querySelector("h1").innerHTML = "Draw";
+}
