2762f5d087eda6ab70045235a91475fde8dfd766
jobairRafi96 <jubairrafi96960@gmail.com>
2020-04-18 07:40:05 +0600

    dice= randomaize pic done

--- DiceGame/index.js
+++ DiceGame/index.js
@@ -1,1 +1,13 @@
 
+var randomNumber1 = Math.floor(Math.random()*6) +1;
+
+var randomNumber2 = Math.floor(Math.random()*6) +1;
+
+var diceimg1 = "dice" + randomNumber1 + ".png"; //pick dice
+var diceimg2 = "dice" + randomNumber2 + ".png";
+
+var imgsrc1 = "images/" + diceimg1;
+var imgsrc2 = "images/" + diceimg2;
+
+document.querySelector(".img1").setAttribute("src",imgsrc1);
+document.querySelector(".img2").setAttribute("src",imgsrc2);
