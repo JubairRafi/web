3060f4e18e71c2fd288f863626836c5df562676f
jobairRafi96 <jubairrafi96960@gmail.com>
2020-04-20 15:11:14 +0600

    Rock = auto first refresh solved using click

--- RockPaper/index.js
+++ RockPaper/index.js
@@ -1,25 +1,29 @@
-var random1 = Math.floor(Math.random()*3)+1;
-var random2 = Math.floor(Math.random()*3)+1;
+document.addEventListener("click",function(){
 
-var movePic1 = "move" + random1 + ".png";
-var movePic2 = "move" + random2 + ".png";
+  var random1 = Math.floor(Math.random()*3)+1;
+  var random2 = Math.floor(Math.random()*3)+1;
 
-var source1 = "images/" + movePic1;
-var source2 = "images/" + movePic2;
+  var movePic1 = "move" + random1 + ".png";
+  var movePic2 = "move" + random2 + ".png";
 
-document.querySelector(".img1").setAttribute("src",source1);
-document.querySelector(".img2").setAttribute("src",source2);
+  var source1 = "images/" + movePic1;
+  var source2 = "images/" + movePic2;
 
-if((random1 == 1 && random2 == 2) || (random1 == 2 && random2 == 3) || (random1 == 3 && random2 == 1)){
-  document.querySelector("h3").innerHTML = "Refresh";
-  document.querySelector("h1").innerHTML = "Player 2 wins";
-}
-  else if ((random1 == 1 && random2 == 3) || ( random1 == 2 && random2 == 1) || (random1 == 3 && random2 == 2)) {
-    document.querySelector("h3").innerHTML = "Refresh";
-    document.querySelector("h1").innerHTML = "Player 1 wins";
-  }
+  document.querySelector(".img1").setAttribute("src",source1);
+  document.querySelector(".img2").setAttribute("src",source2);
 
-  else{
-    document.querySelector("h3").innerHTML = "Refresh";
-    document.querySelector("h1").innerHTML = "Draw!!";
-  }
+      if((random1 == 1 && random2 == 2) || (random1 == 2 && random2 == 3) || (random1 == 3 && random2 == 1)){
+        document.querySelector("h3").innerHTML = "Click Anywhere to Play";
+        document.querySelector("h1").innerHTML = "Player 2 wins";
+      }
+      else if ((random1 == 1 && random2 == 3) || ( random1 == 2 && random2 == 1) || (random1 == 3 && random2 == 2)) {
+        document.querySelector("h3").innerHTML = "Click Anywhere to Play";
+        document.querySelector("h1").innerHTML = "Player 1 wins";
+      }
+
+      else{
+        document.querySelector("h3").innerHTML = "Click Anywhere to Play";
+        document.querySelector("h1").innerHTML = "Draw!!";
+      }
+
+});
