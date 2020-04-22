38ecccc875b3b7ef34706dbb5063be4ef8791f20
jobairRafi96 <jubairrafi96960@gmail.com>
2020-04-22 07:56:06 +0600

    Tic = game logic if else added and move selection added

--- TicTacTo/index.js
+++ TicTacTo/index.js
@@ -1,13 +1,103 @@
 
-
+var myKey =prompt("enter you choice");
 document.addEventListener("keypress",function(event){
   if (event.key=="w") {
-      for (var i = 0; i < document.querySelectorAll(".key").length; i++) {
-        if (document.querySelectorAll(".key")[i].textContent == "w" ) {
-          document.querySelectorAll(".key")[i].textContent ="o";
-        }
-      }
-  } else {
-      alert("wrong input");
+
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i+1].textContent == "myKey" && key[i+2].textContent == "myKey") ||(key[i+3].textContent == "myKey" && key[i+6].textContent == "myKey") ||(key[i+4].textContent == "myKey" && key[i+8].textContent == "myKey")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="a") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-1].textContent == "o" && key[i+1].textContent == "o") ||(key[i+3].textContent == "o" && key[i+6].textContent == "o")  ) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="s") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-1].textContent == "o" && key[i-2].textContent == "o") ||(key[i+3].textContent == "o" && key[i+6].textContent == "o")||(key[i+4].textContent == "o" && key[i+2].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="d") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i+1].textContent == "o" && key[i+2].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="f") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")||(key[i-i].textContent == "o" && key[i+i].textContent == "o") ||(key[i+2].textContent == "o" && key[i-2].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="g") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="h") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-i].textContent == "o" && key[i-3].textContent == "o") ||(key[i+1].textContent == "o" && key[i+2].textContent == "o") ||(key[i+4].textContent == "o" && key[i+2].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="j") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-6].textContent == "o" && key[i-3].textContent == "o") ||(key[i-1].textContent == "o" && key[i+1].textContent == "o")) {
+              alert("game win 0");
+          }
+
+  } else if (event.key=="k") {
+          let i = doMove();
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-i].textContent == "o" && key[i-4].textContent == "o") ||(key[i-3].textContent == "o" && key[i-6].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")) {
+              alert("game win 0");
+          }
+
   }
+
+  function doMove(){
+    for (var i = 0; i < document.querySelectorAll(".key").length; i++) {
+      if (document.querySelectorAll(".key")[i].textContent == event.key ) {
+        document.querySelectorAll(".key")[i].textContent ="o";
+        return i;
+      }
+    }
+  }
+
+
+
+
+
+
+
 });
