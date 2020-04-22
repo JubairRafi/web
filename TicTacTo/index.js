cfd5aa9c13ba5d2513e04c66c2689ef053ac01a1
jobairRafi96 <jubairrafi96960@gmail.com>
2020-04-22 17:59:06 +0600

    double player added

--- TicTacTo/index.js
+++ TicTacTo/index.js
@@ -1,103 +1,150 @@
 
-var myKey =prompt("enter you choice");
+var myKey1 =prompt("enter player1's choice");
+var myKey2 =prompt("enter player2's choice");
+var flag = 0;
 document.addEventListener("keypress",function(event){
-  if (event.key=="w") {
 
-          let i = doMove();
 
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i+1].textContent == "myKey" && key[i+2].textContent == "myKey") ||(key[i+3].textContent == "myKey" && key[i+6].textContent == "myKey") ||(key[i+4].textContent == "myKey" && key[i+8].textContent == "myKey")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="a") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-1].textContent == "o" && key[i+1].textContent == "o") ||(key[i+3].textContent == "o" && key[i+6].textContent == "o")  ) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="s") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-1].textContent == "o" && key[i-2].textContent == "o") ||(key[i+3].textContent == "o" && key[i+6].textContent == "o")||(key[i+4].textContent == "o" && key[i+2].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="d") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i+1].textContent == "o" && key[i+2].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="f") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")||(key[i-i].textContent == "o" && key[i+i].textContent == "o") ||(key[i+2].textContent == "o" && key[i-2].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="g") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-3].textContent == "o" && key[i+3].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="h") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-i].textContent == "o" && key[i-3].textContent == "o") ||(key[i+1].textContent == "o" && key[i+2].textContent == "o") ||(key[i+4].textContent == "o" && key[i+2].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="j") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-6].textContent == "o" && key[i-3].textContent == "o") ||(key[i-1].textContent == "o" && key[i+1].textContent == "o")) {
-              alert("game win 0");
-          }
-
-  } else if (event.key=="k") {
-          let i = doMove();
-
-          let key =document.querySelectorAll(".key");
-
-          if ((key[i-i].textContent == "o" && key[i-4].textContent == "o") ||(key[i-3].textContent == "o" && key[i-6].textContent == "o") ||(key[i-1].textContent == "o" && key[i-2].textContent == "o")) {
-              alert("game win 0");
-          }
-
+  if (flag==0) {
+    player1();
+  } else {
+    player2();
   }
-
-  function doMove(){
-    for (var i = 0; i < document.querySelectorAll(".key").length; i++) {
-      if (document.querySelectorAll(".key")[i].textContent == event.key ) {
-        document.querySelectorAll(".key")[i].textContent ="o";
-        return i;
-      }
-    }
-  }
-
-
-
-
-
 
 
 });
+
+function player1(){
+  flag = 1;
+  var player1win = gameLogic(myKey1);
+  if (player1win) {
+      alert("player1 wins");
+   }
+}
+
+function player2(){
+  flag = 0;
+  var player2win = gameLogic(myKey2);
+  if (player2win) {
+     alert("player2 wins");
+  }
+}
+
+
+function gameLogic(myKey){
+
+  if (event.key=="w") {
+
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i+1].textContent == myKey && key[i+2].textContent == myKey) ||(key[i+3].textContent == myKey && key[i+6].textContent == myKey) ||(key[i+4].textContent == myKey && key[i+8].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="a") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-1].textContent == myKey && key[i+1].textContent == myKey) ||(key[i+3].textContent == myKey && key[i+6].textContent == myKey)  ) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="s") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-1].textContent == myKey && key[i-2].textContent == myKey) ||(key[i+3].textContent == myKey && key[i+6].textContent == myKey)||(key[i+4].textContent == myKey && key[i+2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="d") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == myKey && key[i+3].textContent == myKey) ||(key[i+1].textContent == myKey && key[i+2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="f") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == myKey && key[i+3].textContent == myKey) ||(key[i-1].textContent == myKey && key[i-2].textContent == myKey)||(key[i-i].textContent == myKey && key[i+i].textContent == myKey) ||(key[i+2].textContent == myKey && key[i-2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="g") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-3].textContent == myKey && key[i+3].textContent == myKey) ||(key[i-1].textContent == myKey && key[i-2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="h") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-i].textContent == myKey && key[i-3].textContent == myKey) ||(key[i+1].textContent == myKey && key[i+2].textContent == myKey) ||(key[i+4].textContent == myKey && key[i+2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="j") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-6].textContent == myKey && key[i-3].textContent == myKey) ||(key[i-1].textContent == myKey && key[i+1].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else if (event.key=="k") {
+          let i = doMove(myKey);
+
+          let key =document.querySelectorAll(".key");
+
+          if ((key[i-i].textContent == myKey && key[i-4].textContent == myKey) ||(key[i-3].textContent == myKey && key[i-6].textContent == myKey) ||(key[i-1].textContent == myKey && key[i-2].textContent == myKey)) {
+              return true;
+          }else{
+              return false;
+          }
+
+  } else{
+    alert("please enter right key");
+  }
+
+}
+
+
+function doMove(myKey){
+  for (var i = 0; i < document.querySelectorAll(".key").length; i++) {
+    if (document.querySelectorAll(".key")[i].textContent == event.key ) {
+      document.querySelectorAll(".key")[i].textContent =myKey;
+      return i;
+    }
+  }
+}
